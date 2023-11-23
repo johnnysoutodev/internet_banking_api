b4b10dfd3ec90e5ff5d58ac68ddf1590a96a54ed
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-23 17:09:04 -0300

    init project
    iniciando o projeto

---
+++ src/main/java/br/com/santander/internet_banking_api/InternetBankingApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.santander.internet_banking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetBankingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetBankingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/santander/internet_banking_api/InternetBankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.santander.internet_banking_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetBankingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
