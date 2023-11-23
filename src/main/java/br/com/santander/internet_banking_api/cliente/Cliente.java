1c8771e242b6c7b7e3da76f6700543a65df95878
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-23 17:57:51 -0300

    Criando a conta e o cliente

---
+++ src/main/java/br/com/santander/internet_banking_api/cliente/Cliente.java
@@ -0,0 +1,11 @@
+package br.com.santander.internet_banking_api.cliente;
+
+import br.com.santander.internet_banking_api.conta.Conta;
+import java.util.Date;
+
+public class Cliente {
+
+    private String nome;
+    private Date dataNascimento;
+    private Conta conta;
+}
---
+++ src/main/java/br/com/santander/internet_banking_api/conta/Conta.java
@@ -0,0 +1,9 @@
+package br.com.santander.internet_banking_api.conta;
+
+import java.math.BigDecimal;
+
+public class Conta {
+    private String numeroDaConta;
+    private Boolean planoExclusive;
+    private BigDecimal saldo;
+}
