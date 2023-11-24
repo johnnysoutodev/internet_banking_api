7024ae928345e9b9846021cbf223b4b731c52513
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-24 05:17:24 -0300

    Update Conta.java

--- src/main/java/br/com/santander/internet_banking_api/conta/Conta.java
+++ src/main/java/br/com/santander/internet_banking_api/conta/Conta.java
@@ -1,10 +1,16 @@
 package br.com.santander.internet_banking_api.conta;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Conta {
     private String numero_da_conta;
     private Boolean plano_exclusive;
