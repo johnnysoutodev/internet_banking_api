cdb31d41f597836f921ece37cab705780279906e
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-24 04:40:41 -0300

    Update ClienteController.java
    importando as notações

--- src/main/java/br/com/santander/internet_banking_api/controller/ClienteController.java
+++ src/main/java/br/com/santander/internet_banking_api/controller/ClienteController.java
@@ -1,9 +1,17 @@
 package br.com.santander.internet_banking_api.controller;
 
+import br.com.santander.internet_banking_api.cliente.DadosCadastroCliente;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("clientes")
 public class ClienteController {
+
+    @PostMapping
+    public void cadastrar(@RequestBody DadosCadastroCliente dados){
+        System.out.println(dados);
+    }
 }
