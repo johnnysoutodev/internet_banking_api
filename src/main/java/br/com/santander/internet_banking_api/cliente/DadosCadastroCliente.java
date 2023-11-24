f65faf05d61634db20a6ea71ecc455ef1832e9f2
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-24 06:38:11 -0300

    Update DadosCadastroCliente.java
    validando os campos

--- src/main/java/br/com/santander/internet_banking_api/cliente/DadosCadastroCliente.java
+++ src/main/java/br/com/santander/internet_banking_api/cliente/DadosCadastroCliente.java
@@ -1,12 +1,17 @@
 package br.com.santander.internet_banking_api.cliente;
 
 import br.com.santander.internet_banking_api.conta.DadosConta;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 
 public record DadosCadastroCliente(
+        @NotBlank
         String nome,
+        @NotBlank
         String data_de_nascimento,
-        DadosConta conta
+        @NotNull @Valid DadosConta conta
 ) {
 }
