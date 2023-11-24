b9197e796189fe475aa736d1e35f9c642706076f
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-24 05:15:57 -0300

    Update Cliente.java

--- src/main/java/br/com/santander/internet_banking_api/cliente/Cliente.java
+++ src/main/java/br/com/santander/internet_banking_api/cliente/Cliente.java
@@ -1,4 +1,15 @@
 package br.com.santander.internet_banking_api.cliente;
 
+import br.com.santander.internet_banking_api.conta.Conta;
+import jakarta.persistence.*;
+
+@Table(name = "clientes")
+@Entity(name = "Cliente")
 public class Cliente {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String data_de_nascimento;
+    @Embedded
+    Conta conta;
 }
