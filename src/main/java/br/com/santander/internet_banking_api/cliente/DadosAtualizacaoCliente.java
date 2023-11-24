1ef73cf5a2fe88af37be7b2074510cbf26f74c45
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-24 12:49:00 -0300

    Update DadosAtualizacaoCliente.java

--- src/main/java/br/com/santander/internet_banking_api/cliente/DadosAtualizacaoCliente.java
+++ src/main/java/br/com/santander/internet_banking_api/cliente/DadosAtualizacaoCliente.java
@@ -1,4 +1,13 @@
 package br.com.santander.internet_banking_api.cliente;
 
-public record DadosAtualizacaoCliente() {
+import br.com.santander.internet_banking_api.conta.DadosConta;
+import jakarta.validation.constraints.NotNull;
+
+public record DadosAtualizacaoCliente(
+        @NotNull
+        Long id,
+        String nome,
+        String data_de_nascimento,
+        DadosConta conta
+) {
 }
