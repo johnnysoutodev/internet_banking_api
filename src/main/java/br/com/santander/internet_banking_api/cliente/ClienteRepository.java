20a2015287a38b53df15f0d5d6c71c198e77fa60
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-24 04:28:50 -0300

    Delete ClienteRepository.java

--- src/main/java/br/com/santander/internet_banking_api/cliente/ClienteRepository.java
+++
@@ -1,6 +0,0 @@
-package br.com.santander.internet_banking_api.cliente;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
