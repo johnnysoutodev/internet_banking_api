157fd2f1c29a546ddc2aa5073264159b64ced86c
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-24 06:38:17 -0300

    Update DadosConta.java
    validando os campos

--- src/main/java/br/com/santander/internet_banking_api/conta/DadosConta.java
+++ src/main/java/br/com/santander/internet_banking_api/conta/DadosConta.java
@@ -1,10 +1,18 @@
 package br.com.santander.internet_banking_api.conta;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 import java.math.BigDecimal;
 
 public record DadosConta(
+        @NotBlank
+        @Pattern(regexp = "\\d{6}")
         String numero_da_conta,
+        @NotNull
         Boolean plano_exclusive,
+        @NotNull
         BigDecimal saldo
 ) {
 }
