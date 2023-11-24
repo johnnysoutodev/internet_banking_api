2496f179eaa992664f4260da6d9d3b6c76dea94b
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-24 04:40:24 -0300

    Update DadosCadastroCliente.java

--- src/main/java/br/com/santander/internet_banking_api/cliente/DadosCadastroCliente.java
+++ src/main/java/br/com/santander/internet_banking_api/cliente/DadosCadastroCliente.java
@@ -1,4 +1,12 @@
 package br.com.santander.internet_banking_api.cliente;
 
-public record DadosCadastroCliente() {
+import java.math.BigDecimal;
+
+public record DadosCadastroCliente(
+        String nome,
+        String data_de_nascimento,
+        String numero_da_conta,
+        Boolean plano_exclusive,
+        BigDecimal saldo
+) {
 }
