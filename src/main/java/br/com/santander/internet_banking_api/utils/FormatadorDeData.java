5eed5f2e291335b3c6d01eaf2ae917fc7a0e5251
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-24 16:11:13 -0300

    Update FormatadorDeData.java

--- src/main/java/br/com/santander/internet_banking_api/utils/FormatadorDeData.java
+++ src/main/java/br/com/santander/internet_banking_api/utils/FormatadorDeData.java
@@ -1,4 +1,17 @@
 package br.com.santander.internet_banking_api.utils;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class FormatadorDeData {
+
+    private String strData;
+
+    private static final DateTimeFormatter FORMATO_BRASILEIRO =
+            DateTimeFormatter.ofPattern("dd/MM/yyyy", new Locale("pt", "BR"));
+
+    public static LocalDate converterStringParaData(String strData) {
+        return LocalDate.parse(strData, FORMATO_BRASILEIRO);
+    }
 }
