9b5e78b6995d7867112fcfbfa4ae8de2eee4cf62
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-24 13:43:57 -0300

    Update ClienteRepository.java
    o metodo excluir precisou desta interface

--- src/main/java/br/com/santander/internet_banking_api/cliente/ClienteRepository.java
+++ src/main/java/br/com/santander/internet_banking_api/cliente/ClienteRepository.java
@@ -1,6 +1,11 @@
 package br.com.santander.internet_banking_api.cliente;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    Page<Cliente> findAllByAtivoTrue(Pageable paginacao);
 }
