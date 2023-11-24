d1b319bbb60cf3f0b9dbdbecfdd6b75bddd92e3d
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-24 04:42:59 -0300

    Update DadosCadastroCliente.java
    separando os dados da conta

--- src/main/java/br/com/santander/internet_banking_api/cliente/DadosCadastroCliente.java
+++ src/main/java/br/com/santander/internet_banking_api/cliente/DadosCadastroCliente.java
@@ -1,12 +1,12 @@
 package br.com.santander.internet_banking_api.cliente;
 
+import br.com.santander.internet_banking_api.conta.DadosConta;
+
 import java.math.BigDecimal;
 
 public record DadosCadastroCliente(
         String nome,
         String data_de_nascimento,
-        String numero_da_conta,
-        Boolean plano_exclusive,
-        BigDecimal saldo
+        DadosConta conta
 ) {
 }
