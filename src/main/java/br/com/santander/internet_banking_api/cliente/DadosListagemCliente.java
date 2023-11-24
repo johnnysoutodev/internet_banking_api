d2f9a9aa077344e61b63310f154e0702585f9023
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-24 10:46:46 -0300

    Update DadosListagemCliente.java

--- src/main/java/br/com/santander/internet_banking_api/cliente/DadosListagemCliente.java
+++ src/main/java/br/com/santander/internet_banking_api/cliente/DadosListagemCliente.java
@@ -1,4 +1,11 @@
 package br.com.santander.internet_banking_api.cliente;
 
-public record DadosListagemCliente() {
+public record DadosListagemCliente(
+        String nome,
+        String numero_da_conta,
+        Boolean plano_exclusive
+) {
+    public DadosListagemCliente(Cliente cliente){
+        this(cliente.getNome(), cliente.getNumero_da_conta(), cliente.getPlano_exclusive());
+    }
 }
