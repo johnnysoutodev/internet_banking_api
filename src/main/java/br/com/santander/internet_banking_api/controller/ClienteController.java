a2663cd21a8e8cff6b0e97e10cd8259191939e05
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-24 04:30:49 -0300

    Create ClienteController.java
    add anotações

---
+++ src/main/java/br/com/santander/internet_banking_api/controller/ClienteController.java
@@ -0,0 +1,9 @@
+package br.com.santander.internet_banking_api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("clientes")
+public class ClienteController {
+}
