0afb9d962411952b4300fc768492c3c2a422b66c
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-24 06:22:57 -0300

    Update V1__create-table-clientes.sql

---
+++ src/main/resources/db/migration/V1__create-table-clientes.sql
@@ -0,0 +1,9 @@
+create table clientes (
+    id bigint not null auto_increment,
+    nome varchar(100) not null,
+    data_de_nascimento varchar(100) not null,
+    numero_da_conta varchar(10) not null unique,
+    plano_exclusive boolean not null,
+    saldo float not null,
+    primary key (id)
+);
