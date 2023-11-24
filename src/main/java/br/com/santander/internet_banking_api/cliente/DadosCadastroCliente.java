c94f1a9fd2814f404c00b74c8cc651eccd7008fd
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-24 15:52:35 -0300

    Update DadosCadastroCliente.java
    regex para validar a data_de_nascimento

--- src/main/java/br/com/santander/internet_banking_api/cliente/DadosCadastroCliente.java
+++ src/main/java/br/com/santander/internet_banking_api/cliente/DadosCadastroCliente.java
@@ -4,6 +4,7 @@ import br.com.santander.internet_banking_api.conta.DadosConta;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 import java.math.BigDecimal;
 
@@ -11,6 +12,7 @@ public record DadosCadastroCliente(
         @NotBlank
         String nome,
         @NotBlank
+        @Pattern(regexp = "^\\d{2}/\\d{2}/\\d{4}$")
         String data_de_nascimento,
         @NotNull @Valid DadosConta conta
 ) {
