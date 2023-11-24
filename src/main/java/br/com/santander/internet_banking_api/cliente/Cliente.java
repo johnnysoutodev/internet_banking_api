d66b4f27101d98ffe50cb99137cf53344b224307
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-24 05:16:57 -0300

    Update Cliente.java

--- src/main/java/br/com/santander/internet_banking_api/cliente/Cliente.java
+++ src/main/java/br/com/santander/internet_banking_api/cliente/Cliente.java
@@ -2,9 +2,17 @@ package br.com.santander.internet_banking_api.cliente;
 
 import br.com.santander.internet_banking_api.conta.Conta;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Table(name = "clientes")
 @Entity(name = "Cliente")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Cliente {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
