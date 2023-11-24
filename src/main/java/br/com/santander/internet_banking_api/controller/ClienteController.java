8250b6999b9c945aa5431441518c27f19cad48b6
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-24 00:24:05 -0300

    Update ClienteController.java

--- src/main/java/br/com/santander/internet_banking_api/controller/ClienteController.java
+++ src/main/java/br/com/santander/internet_banking_api/controller/ClienteController.java
@@ -5,6 +5,7 @@ import br.com.santander.internet_banking_api.cliente.ClienteRepository;
 import br.com.santander.internet_banking_api.cliente.DadosCadastroCliente;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,6 +15,7 @@ public class ClienteController {
     private ClienteRepository repository;
 
     @PostMapping
+    @Transactional
     public void cadastrar(@RequestBody DadosCadastroCliente dados){
         repository.save(new Cliente(dados));
     }
