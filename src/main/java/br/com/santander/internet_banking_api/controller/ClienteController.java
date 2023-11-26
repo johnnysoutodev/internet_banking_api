05ffbffcb6c76fe8e0c99fdf0a706c4ae0cfeaf2
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-26 18:10:11 -0300

    Revert "Update ClienteController.java"
    This reverts commit a9cdc56173eba3c905c99748b796e7c04405cf19.

--- src/main/java/br/com/santander/internet_banking_api/controller/ClienteController.java
+++ src/main/java/br/com/santander/internet_banking_api/controller/ClienteController.java
@@ -8,10 +8,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("clientes")
@@ -21,29 +19,20 @@ public class ClienteController {
 
     @PostMapping
     @Transactional
-    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroCliente dados, UriComponentsBuilder uriBuilder){
-        var cliente = new Cliente(dados);
-
-        repository.save(cliente);
-
-        var uri = uriBuilder.path("clientes/{id}").buildAndExpand(cliente.getId()).toUri();
-
-        return ResponseEntity.created(uri).body(new DadosDetalhamentoCliente(cliente));
+    public void cadastrar(@RequestBody @Valid DadosCadastroCliente dados){
+        repository.save(new Cliente(dados));
     }
 
     @GetMapping
-    public ResponseEntity <Page<DadosListagemCliente>> listar(@PageableDefault(size = 10, page = 0, sort = {"id"}) Pageable paginacao){
-        var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemCliente::new);
-        return ResponseEntity.of(Optional.of(page));
+    public Page<DadosListagemCliente> listar(@PageableDefault(size = 10, page = 0, sort = {"id"}) Pageable paginacao){
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemCliente::new);
     }
 
     @PutMapping
     @Transactional
-    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoCliente dados){
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoCliente dados){
         var cliente = repository.getReferenceById(dados.id());
         cliente.atualizarInformacoes(dados);
-
-        return ResponseEntity.ok(new DadosDetalhamentoCliente(cliente));
     }
 
     @DeleteMapping("/{id}")
