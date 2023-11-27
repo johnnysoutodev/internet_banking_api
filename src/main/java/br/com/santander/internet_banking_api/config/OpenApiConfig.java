3f210dee6e865902e7b621e82a046b3ebcf7dc24
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-26 22:21:58 -0300

    Create OpenApiConfig.java

---
+++ src/main/java/br/com/santander/internet_banking_api/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package br.com.santander.internet_banking_api.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Johnny Souto",
+                        email = "johnny.souto@cagpgemini.com ",
+                        url = "https://wwww.capgemini.com.br"
+                ),
+                
+        )
+)
+public class OpenApiConfig {
+}
