660f26f2ac6f8c431fe0f2907c89f0163d030a86
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-27 13:42:38 -0300

    Update DadosAtualizacaoCliente.java
    utilizando apenas um DTO

--- src/main/java/br/com/santander/internet_banking_api/domain/cliente/DadosAtualizacaoCliente.java
+++ src/main/java/br/com/santander/internet_banking_api/domain/cliente/DadosAtualizacaoCliente.java
@@ -1,13 +1,19 @@
 package br.com.santander.internet_banking_api.domain.cliente;
 
-import br.com.santander.internet_banking_api.domain.conta.DadosConta;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+import java.math.BigDecimal;
 
 public record DadosAtualizacaoCliente(
-        @NotNull
         Long id,
         String nome,
+        @Pattern(regexp = "^\\d{2}/\\d{2}/\\d{4}$")
         String data_de_nascimento,
-        DadosConta conta
+        @Pattern(regexp = "\\d{6}")
+        String numero_da_conta,
+        Boolean plano_exclusive,
+        BigDecimal saldo
 ) {
 }
