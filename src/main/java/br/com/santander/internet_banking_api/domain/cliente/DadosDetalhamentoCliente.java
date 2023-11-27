2e88add1f76d1d179a7d97a7bc008f2ba3e7d586
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-27 13:42:07 -0300

    Update DadosDetalhamentoCliente.java
    utilizando apenas um DTO

--- src/main/java/br/com/santander/internet_banking_api/domain/cliente/DadosDetalhamentoCliente.java
+++ src/main/java/br/com/santander/internet_banking_api/domain/cliente/DadosDetalhamentoCliente.java
@@ -1,11 +1,24 @@
 package br.com.santander.internet_banking_api.domain.cliente;
 
+
+import java.math.BigDecimal;
+
 public record DadosDetalhamentoCliente(
         Long id,
         String nome,
-        String data_de_nascimento) {
+        String data_de_nascimento,
+        String numero_da_conta,
+        Boolean plano_exclusive,
+        BigDecimal saldo) {
 
     public DadosDetalhamentoCliente(Cliente cliente){
-        this(cliente.getId(), cliente.getNome(), cliente.getData_de_nascimento());
+        this(
+                cliente.getId(),
+                cliente.getNome(),
+                cliente.getData_de_nascimento(),
+                cliente.getNumero_da_conta(),
+                cliente.getPlano_exclusive(),
+                cliente.getSaldo()
+        );
     }
 }
