420a9edf4770abaa2f84e8cf18bac664c6cf84a3
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-27 13:41:41 -0300

    Update ClienteController.java
    utilizando apenas um DTO

--- src/main/java/br/com/santander/internet_banking_api/controller/ClienteController.java
+++ src/main/java/br/com/santander/internet_banking_api/controller/ClienteController.java
@@ -3,6 +3,7 @@ package br.com.santander.internet_banking_api.controller;
 import br.com.santander.internet_banking_api.domain.cliente.*;
 import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/cliente")
@@ -21,7 +24,13 @@ public class ClienteController {
 
     @Operation(
             description = "Endpoint do tipo POST para cadastrar cliente.",
-            summary = "Cadastrar cliente."
+            summary = "Cadastrar cliente.",
+            responses ={
+                    @ApiResponse(
+                            description = "Criado.",
+                            responseCode = "201"
+                    )
+            }
     )
     @PostMapping("/cadastrar")
     @Transactional
@@ -34,8 +43,8 @@ public class ClienteController {
             summary = "Lista todos os clientes."
     )
     @GetMapping("/listarTodos")
-    public Page<DadosListagemCliente> listar(@PageableDefault(size = 10, page = 0, sort = {"id"}) Pageable paginacao){
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemCliente::new);
+    public Page<DadosDetalhamentoCliente> listar(@PageableDefault(size = 10, page = 0, sort = {"id"}) Pageable paginacao){
+        return repository.findAllByAtivoTrue(paginacao).map(DadosDetalhamentoCliente::new);
     }
 
     @Operation(
@@ -60,5 +69,14 @@ public class ClienteController {
     public void excluir(@PathVariable Long id){
         var cliente = repository.getReferenceById(id);
         cliente.excluir();
+    }
+
+    @Operation(
+            description = "Endpoint do tipo GET para detalhar um cliente.",
+            summary = "Detalhar cliente."
+    )
+    @GetMapping("/detalhar/{id}")
+    public List<DadosDetalhamentoCliente> detalhar(@PathVariable Long id) {
+        return repository.findById(id).stream().map(DadosDetalhamentoCliente::new).toList();
     }
 }
