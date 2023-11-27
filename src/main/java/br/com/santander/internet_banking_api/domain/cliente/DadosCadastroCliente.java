7a9c8335038e0b51630d9bc0d108af02c4b58f7e
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-27 13:42:58 -0300

    Update DadosCadastroCliente.java
    ajustando validacao

--- src/main/java/br/com/santander/internet_banking_api/domain/cliente/DadosCadastroCliente.java
+++ src/main/java/br/com/santander/internet_banking_api/domain/cliente/DadosCadastroCliente.java
@@ -1,10 +1,10 @@
 package br.com.santander.internet_banking_api.domain.cliente;
 
-import br.com.santander.internet_banking_api.domain.conta.DadosConta;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+
+import java.math.BigDecimal;
 
 public record DadosCadastroCliente(
         @NotBlank
@@ -12,6 +12,12 @@ public record DadosCadastroCliente(
         @NotBlank
         @Pattern(regexp = "^\\d{2}/\\d{2}/\\d{4}$")
         String data_de_nascimento,
-        @NotNull @Valid DadosConta conta
+        @NotBlank
+        @Pattern(regexp = "\\d{6}")
+        String numero_da_conta,
+        @NotNull
+        Boolean plano_exclusive,
+        @NotNull
+        BigDecimal saldo
 ) {
 }
