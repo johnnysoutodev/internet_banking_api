19b06772e07c67315ee1a6663231d183fcff1c0a
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-27 11:32:26 -0300

    Update DadosDetalhamentoCliente.java

--- src/main/java/br/com/santander/internet_banking_api/domain/cliente/DadosDetalhamentoCliente.java
+++ src/main/java/br/com/santander/internet_banking_api/domain/cliente/DadosDetalhamentoCliente.java
@@ -1,6 +1,9 @@
 package br.com.santander.internet_banking_api.domain.cliente;
 
-public record DadosDetalhamentoCliente(Long id, String nome, String data_de_nascimento) {
+public record DadosDetalhamentoCliente(
+        Long id,
+        String nome,
+        String data_de_nascimento) {
 
     public DadosDetalhamentoCliente(Cliente cliente){
         this(cliente.getId(), cliente.getNome(), cliente.getData_de_nascimento());
