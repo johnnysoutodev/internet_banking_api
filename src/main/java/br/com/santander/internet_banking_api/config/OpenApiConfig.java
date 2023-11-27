afa7090e411a557202fc10c5660591f6fc2f7648
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-26 22:33:28 -0300

    Update OpenApiConfig.java
    end of the info definition

--- src/main/java/br/com/santander/internet_banking_api/config/OpenApiConfig.java
+++ src/main/java/br/com/santander/internet_banking_api/config/OpenApiConfig.java
@@ -3,6 +3,7 @@ package br.com.santander.internet_banking_api.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -12,7 +13,13 @@ import io.swagger.v3.oas.annotations.info.Info;
                         url = "https://www.capgemini.com.br"
                 ),
                 description = "API do teste para desenvolvedor Back-End do Santander",
-                title = "Santander Banking API"
+                title = "Santander Banking API",
+                version = "1.0.0",
+                license = @License(
+                        name = "License Name",
+                        url = "https://www.licensesite.com"
+                ),
+                termsOfService = "Terms of service"
         )
 )
 public class OpenApiConfig {
