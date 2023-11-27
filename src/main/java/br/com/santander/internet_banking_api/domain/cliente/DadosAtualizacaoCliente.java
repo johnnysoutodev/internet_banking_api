c7750257a53aeac3e6a33d8239bf67ef8d6a5c04
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2023-11-27 15:01:09 -0300

    Update DadosAtualizacaoCliente.java

--- src/main/java/br/com/santander/internet_banking_api/domain/cliente/DadosAtualizacaoCliente.java
+++ src/main/java/br/com/santander/internet_banking_api/domain/cliente/DadosAtualizacaoCliente.java
@@ -10,8 +10,8 @@ import java.util.Date;
 public record DadosAtualizacaoCliente(
         Long id,
         String nome,
-        @Pattern(regexp = "^\\d{2}/\\d{2}/\\d{4}$")
-        Date data_de_nascimento,
+        @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$")
+        String data_de_nascimento,
         @Pattern(regexp = "\\d{6}")
         String numero_da_conta,
         Boolean plano_exclusive,
